6d431b9c3327f45b07ddd0955c26d16b7ad88a08
Iyadb Horani <i3d@ironic3d.com>
2014-04-21 09:08:09 +0000

    Add image uploaded with Paperclip

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 gem 'devise', '~> 3.2.4'
+gem 'paperclip', '~> 4.0'
 
 group :doc do
   gem 'sdoc', require: false
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,6 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
+
+  has_attached_file :image, :styles => { :high => "480x480>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.jpg"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
