123f82faf1dc2ddc3d321669b141b3d32a97e283
Iyadb Horani <i3d@ironic3d.com>
2014-04-23 05:53:02 +0000

    Added names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :high => "480x480>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  validates :image, presence: true
+  validates :description, presence: true
 end
